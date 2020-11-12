5cd3bc43852c68776c3a476a5e7a313cf9ac59f4
Michelle <michelle.npm3108@gmail.com>
2020-11-13 06:24:34 +0800

    learn how to console log when coding in VS

---
+++ index.js
@@ -0,0 +1,8 @@
+// in JS you can store values in something called variable 
+// when you DECLARE a variable, its name must be unique. 
+// You can ASSIGN a VALUE to your VARIABLE. The value can be string, array or boolean
+let age = 23 //value here is 23, variable is age
+// variable can be varied, by grabbing the variable and assign it a new value
+age = 24 
+
+console.log(age);   
