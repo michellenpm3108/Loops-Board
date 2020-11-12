268d03c2ccc6eed972d0cf4fe704d3fca5a39653
Michelle <michelle.npm3108@gmail.com>
2020-11-13 07:20:59 +0800

    challenge BOOK STORE

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Scrimba-M3-JS
  learn again the basic of JS (module 3)
+
+
+VOCABULARY
+Assign: assign a value to variable 
+Declare: declare a variable 
+Invoque: invoque a function 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// THEORY
 // in JS you can store values in something called variable 
 // when you DECLARE a variable, its name must be unique. 
 // You can ASSIGN a VALUE to your VARIABLE. The value can be string, array or boolean
@@ -6,7 +7,6 @@
 let age = 22
 // value here is 23, variable is age
 // variable can be varied, by grabbing the variable and assign it a new value
-
 age = 30
 
 // CONST
@@ -31,3 +31,27 @@ function globalScope(){
 
 functionScope() //invoque the function
 globalScope()
+
+// CHALLENGE:  BOOOK STORE 
+
+let bookCount = 0 
+function increment() {
+    bookCount +=1
+    console.log('The number of book in our store is ' + bookCount + ' books')
+}
+increment ()
+
+function bookReplace() {
+    let bookSold = ['Gulp', ' The Life of Pi', ' Quiet Power']
+    console.log ('We have just sold ' + bookSold.length + ' books: ' + bookSold)
+
+    let newBook = ['Moby Dick', ' Alice in Wonderland', ' Hungry Caterpillar']
+    console.log ( newBook.length + ' new books available in our store are: ' + newBook)
+}
+bookReplace ()
+
+function textCustomer() {
+    const firstName = 'Mai'
+    console.log ('Hi ' + firstName + ', your book is now in')
+}
+textCustomer()
