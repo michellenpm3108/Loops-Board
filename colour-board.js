34152fbf0ffd174a90e4072096118975cddeca23
Michelle <michelle.npm3108@gmail.com>
2020-11-15 15:05:16 +0800

    use classList.add to change color of the board

--- colour-board.js
+++ colour-board.js
@@ -1,6 +1,8 @@
 const squares = Array.from(document.querySelectorAll('.grid div'))
 for (let i=0 ; i < squares.length ; i++) {
     if (i % 2 === 0) {
-        squares[i].style.backgroundColor = '#D9A362'
+        squares[i].classList.add('even')
+    } else {
+        squares[i].classList.add('odd')
     }
 }
